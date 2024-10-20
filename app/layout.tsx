2b018b869240110f5a3f95f3990f1018ce26e507
paigeingram <paigehingram@gmail.com>
2024-10-20 06:09:36 -0700

    changed title

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,8 @@ import { Nav } from "@/components/Nav";
 import { cn } from "@/utils";
 
 export const metadata: Metadata = {
-  title: "Hume AI - EVI - Next.js Starter",
-  description: "A Next.js starter using Hume AI's Empathic Voice Interface",
+  title: "Hume AI",
+  description: "Your virtual mental wellness companion.",
 };
 
 export default function RootLayout({
